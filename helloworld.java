092083c3e40cf8896898180f537e8584f4c9a38b
kaveri12347896 <vidyakaveri0@gmail.com>
2024-05-08 19:17:08 +0530

    first commit

--- helloworld.java
+++ helloworld.java
@@ -1,6 +1,19 @@
 class hello{
 public static void main(String[]args)
 {
-System.out.println("hello world");
+int a=10;
+int b=5;
+int c=5;
+int x,y;
+try
+{
+x=a/(b-c);
+}
+catch(Arithmetic Exception e)
+{
+System.out.println("division by zero");
+}
+y=a/(b+c);
+System.out.println("y="+y);
 }
 }
